cfc228b75665797aec4d2f7d406390e59277a544
Sravan Dommaraju <sravan@jacobistrategies.com>
2025-11-05 13:25:26 +0000

    Add build for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/sleek-web-3d/", 
   server: {
     host: "::",
     port: 8080,
